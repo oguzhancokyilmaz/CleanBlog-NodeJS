3008c857e3b43c8303ce29603684166254433951
oguzhancokyilmaz <ocokyilmaz@gmail.com>
2022-01-21 01:38:30 +0300

    Added Template Engine And Partials Files

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const blog = { 
-      id: 1, 
-      title: 'Blog title', 
-      description: 'Blog description' 
-    };
+// TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
 
-  res.send(blog);
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
+  app.get('/post', (req, res) => {
+    res.render('post');
+  });
 
 const port = 3000;
 
