8a65b97292986adb009b950f754f83d3a710aa53
oguzhancokyilmaz <ocokyilmaz@gmail.com>
2022-01-22 02:04:08 +0300

    Publish Post. Last Post Appear First Row

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Post');
+const Post = require('./models/Post');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/cleanBlog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); //urldeki datayı okumamızı sağlar
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({}).sort([['dateCreated', -1]]) // son girilen post ilk sergilenir.
+  res.render('index',{
+    posts
+  });
 });
 app.get('/about', (req, res) => {
-    res.render('about');
-  });
-  app.get('/add_post', (req, res) => {
-    res.render('add_post');
-  });
-  app.get('/post', (req, res) => {
-    res.render('post');
-  });
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 
